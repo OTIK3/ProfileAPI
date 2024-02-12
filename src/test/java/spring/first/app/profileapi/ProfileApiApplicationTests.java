a173d6c081cbf20ccb06220074ce4bd6fe3ca2c8
Tim <vtimofej@mail.ru>
2024-02-12 19:10:29 +0300

    Создал новый проект

---
+++ src/test/java/spring/first/app/profileapi/ProfileApiApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.first.app.profileapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
